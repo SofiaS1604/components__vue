b5fa9e01aab9ab83a14944729a3b8c699f7770d9
SofiaS1604 <SofiaS1604@yandex.ru>
2021-05-02 00:24:25 +0300

    Update code

--- src/view/components/InputNumber.vue
+++ src/view/components/InputNumber.vue
@@ -2,20 +2,24 @@
     <div class="number">
         <div class="input__container">
             <input class="input__number"
-                    type="text"
-                    :id="id"
-                    :name="name"
-                    :placeholder="placeholder"
-                    :readonly="readonly"
-                    :disabled="disabled"
-                    :value="this.amount"
-                    @keydown="handleKeyPress"/>
+                   type="text"
+                   :id="id"
+                   :name="name"
+                   :placeholder="placeholder"
+                   :readonly="readonly"
+                   :disabled="disabled"
+                   :value="currentValue"
+                   @keydown.delete="currentValue = currentValue.slice(0, -1)"
+                   @keydown.up="stepChange(stepAmount)"
+                   @keydown.down="stepChange(-stepAmount)"
+                   @input="handleKeyPress"/>
 
             <div class="input__arrows">
                 <div class="input__arrow input__arrow--up" @click="stepChange(stepAmount)">
                     <img src="https://img.icons8.com/plasticine/50/000000/collapse-arrow.png"/>
                 </div>
-                <div class="input__arrow input__arrow--down" @click="stepChange(-stepAmount)" style="transform: rotate(180deg)">
+                <div class="input__arrow input__arrow--down" @click="stepChange(-stepAmount)"
+                     style="transform: rotate(180deg)">
                     <img src="https://img.icons8.com/plasticine/50/000000/collapse-arrow.png"/>
                 </div>
             </div>
@@ -27,7 +31,6 @@
     export default {
         name: 'InputNumber',
         props: [
-            'value',
             'id',
             'name',
             'type',
@@ -37,63 +40,54 @@
             'max',
             'readonly',
             'disabled',
+            'value'
         ],
         data() {
             return {
                 maxAmount: this.max ? parseInt(this.max) : 100,
-                amount: 0,
                 stepAmount: this.step ? parseInt(this.step) : 1,
                 minAmount: this.min ? parseInt(this.min) : 0,
                 stepMin: !!(this.min && this.step),
-                val: ''
+                val: '',
+                currentValue: this.value,
             };
         },
         watch: {
-            amount(value) {
-                this.$emit('input', value);
-            },
-            value(value) {
-                this.amount = value;
-            },
+            val(val) {
+                this.valueInput(val)
+            }
         },
         methods: {
             handleKeyPress(e) {
-                const allowedChars = new RegExp('^[0-9]+$');
-                const keyCode = e.keyCode || e.which;
-                const match = e.key.match(allowedChars);
-
-                keyCode === 38 ? this.stepChange(this.stepAmount) : null;
-                keyCode === 40 ? this.stepChange(-this.stepAmount) : null;
-
-                if (!match && e.keyCode !== 8)
-                    e.preventDefault();
-
-                this.val += match ? match.input : '';
-                this.val = e.keyCode === 8 ? this.val.slice(0, -1) : this.val;
-                return !!match;
+                const allowedChars = new RegExp('^[a-z0-9]+$');
+                const match = allowedChars.test(e.data);
+                this.val = !!match ? this.currentValue + e.data : this.currentValue + '';
             },
+
             stepChange(diff) {
-                this.amount = parseInt(this.val);
+                this.val = parseInt(this.val);
+                if (this.val <= this.maxAmount && this.val >= this.minAmount)
+                    this.val += this.stepMin ? diff - this.val % this.minAmount % diff : diff;
 
-                if(this.amount <= this.maxAmount && this.amount >= this.minAmount){
-                    if(this.stepMin)
-                        this.amount = parseInt(this.amount) + (diff - parseInt(this.amount) % this.minAmount % diff);
-                    else
-                        this.amount = parseInt(this.amount) + diff;
+                if (this.val > this.maxAmount) {
+                    this.val = this.maxAmount;
+                } else if (this.val < this.minAmount) {
+                    this.val = this.minAmount;
                 }
 
-                if (this.amount > this.maxAmount) {
-                    this.amount = this.maxAmount;
-                } else if (this.amount < this.minAmount) {
-                    this.amount = this.minAmount;
-                }
-
-                this.val = this.amount;
                 this.$forceUpdate();
             },
-        },
-        created() {
-            this.amount = this.value;
+
+            valueInput(val) {
+                val += '';
+                let reg = new RegExp('^[a-z]+$');
+                if (!!reg.test(val[val.length - 1]))
+                    val = val.slice(0, -1);
+
+                this.currentValue = val;
+                this.val = this.currentValue;
+                this.$forceUpdate()
+            }
         },
     };
 </script>
