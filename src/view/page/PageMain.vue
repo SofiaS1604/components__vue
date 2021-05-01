3599e7e8e33ec61c6167e0ee42353728ad630318
SofiaS1604 <SofiaS1604@yandex.ru>
2021-05-01 17:27:17 +0300

    Add code

--- src/view/page/PageMain.vue
+++ src/view/page/PageMain.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page__main">
-        <input-number step="2" min="-10" max="5" value="0" />
+        <input-number min="10" step="3" value="0"/>
     </div>
 </template>
 
